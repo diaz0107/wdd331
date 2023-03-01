a67a1240102651cb63ed0b515beb0ddcc2885408
Ben Diaz <bdiaz0107@outlook.com>
2023-03-01 17:28:26 -0500

    index.html

--- w09/sliding-menu/script/script.js
+++ w09/sliding-menu/script/script.js
@@ -1,7 +1,9 @@
 function toggleNav() {
        var updateElement = document.getElementById("menu-icon");
-  var sidebar = document.getElementById("sidebar");
+      var sidebar = document.getElementById("sidebar");
+      var body = document.querySelector("body");
     //toggle adds a class if it's not there or removes it if it is.
     updateElement.classList.toggle("open");
     sidebar.classList.toggle("open");  
+    body.classList.toggle("open");  
 }
